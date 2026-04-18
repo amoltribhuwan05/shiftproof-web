1902eff498a0481199454e47664cab4bcb0002ea
amoltribhuwan05 <amoltribhuwan09@gmail.com>
2026-04-18 17:13:32 +0530

    fix: remove middleware block from open-next config

--- open-next.config.ts
+++ open-next.config.ts
@@ -12,15 +12,4 @@ export default {
     },
   },
   edgeExternals: ["node:crypto"],
-  middleware: {
-    external: true,
-    override: {
-      wrapper: "cloudflare-edge",
-      converter: "edge",
-      proxyExternalRequest: "fetch",
-      incrementalCache: "dummy",
-      tagCache: "dummy",
-      queue: "dummy",
-    },
-  },
 };
