41b0d3eda85fc4e99eeb918c9cbd3b675eb38085
amoltribhuwan05 <amoltribhuwan09@gmail.com>
2026-05-04 18:39:47 +0530

    fix: add static.cloudflareinsights.com to Content-Security-Policy script-src

--- next.config.ts
+++ next.config.ts
@@ -18,7 +18,7 @@ const securityHeaders = [
     key: "Content-Security-Policy",
     value: [
       "default-src 'self'",
-      "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://apis.google.com https://www.gstatic.com https://www.google.com",
+      "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://apis.google.com https://www.gstatic.com https://www.google.com https://static.cloudflareinsights.com",
       "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
       "font-src 'self' https://fonts.gstatic.com",
       "img-src 'self' data: blob: https:",
